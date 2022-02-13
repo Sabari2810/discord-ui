3694afb83ac499c264a404b7847d7c742a81d199
Sabari2810 <imsabarimanikandan@gmail.com>
2022-02-13 22:21:06 +0530

    cursor-pointer

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>Discord</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeaderBanner />
